f7568ecf72f91ded7ffe2afd001361783ffa62d1
aldomagana <aldo.magana78@icloud.com>
2026-02-27 22:15:48 -0600

    Remove tracked Unity cache folders

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     public float speed = 6f;
-
     private Rigidbody2D rb;
     private float lastCollisionTime;
     private float collisionCooldown = 0.1f;
@@ -11,17 +10,19 @@ public class BallMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
-        // Start diagonally
         rb.velocity = new Vector2(1f, 1f).normalized * speed;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-  Vector2 normal = collision.contacts[0].normal;
-    
-  // Reflect velocity based on surface angle
-  rb.velocity = Vector2.Reflect(rb.velocity, normal);
+        if (Time.time - lastCollisionTime < collisionCooldown)
+            return;
+
+        lastCollisionTime = Time.time;
+        Vector2 normal = collision.contacts[0].normal;
+
+        // Reflect velocity based on surface angle
+        rb.velocity = Vector2.Reflect(rb.velocity, normal);
+        rb.velocity = rb.velocity.normalized * speed;
     }
 }
